078738fd46ae8a77a8be1310dc099b79d813086a
PerkoDomen <domen.perko@student.um.si>
2020-12-26 21:47:00 +0100

    Music player setup.

--- src/com/domen/Main.java
+++ src/com/domen/Main.java
@@ -1,8 +1,19 @@
 package com.domen;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        Scanner sc = new Scanner(System.in);
+	    File file = new File("levels.wav");
+	    AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+	    Clip clip = AudioSystem.getClip();
+	    clip.open(stream);
+
+	    clip.start();
+
     }
 }
